7fe2a59bbf36cf2b401b693f7bed4500bfd4d3ae
Francisco Gautero <francisco.gautero@gmail.com>
2021-02-16 16:53:28 -0300

    Commit 01: Comment on progress

---
+++ app/src/main/java/gautero/tuma/memo/model/Comment.java
@@ -0,0 +1,40 @@
+package gautero.tuma.memo.model;
+
+public class Comment {
+    Long idComment;
+    String comment;
+    String usuario;
+    String profilePic;
+
+    public Long getIdComment() {
+        return idComment;
+    }
+
+    public void setIdComment(Long idComment) {
+        this.idComment = idComment;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
+    }
+}
